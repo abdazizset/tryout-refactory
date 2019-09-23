3bc03068bb7595b412226c66cc9460f11922d474
abdazizset <55698133+abdazizset@users.noreply.github.com>
2019-09-23 20:32:25 +0700

    Add files via upload

---
+++ 02.js
@@ -0,0 +1,125 @@
+//01 grade nilai
+var name=prompt ("what is your name?");
+alret ("hallo"+name); 
+//02 grade nilai
+var nilai=prompt ("grade nilai");
+if (nilai >=90 ){
+    document.write("nilai anda A")
+}else if (nilai <=89 && nilai >=80 ){
+    document.write("nilai anda B")
+}else if (nilai <=79 && nilai >=70 ){
+    document.write("nilai anda C")
+}else if (nilai <=69 && nilai >=60 ){
+    document.write("nilai anda D")
+}else if (nilai <=59){
+    document.write("nilai anda E")
+}
+// 03 ganjil genap
+var angka=prompt ("masukan angka-mu!");
+let nomer [angka];
+let i=0;
+for(i,i nomer.lenght; i++){
+    console.log(nomer[i])
+    if (nomer [i] % 2==0){
+        genap.push(nomer[i]);
+    }else{
+        ganjil.push(nomer[i])
+    }
+}
+console.log('Hasil Filter Angka Genap adalah'+genap)
+console.log('Hasil Filter Angka Ganjil adalah'+ganjil)
+// 04 kabisat atau tidak
+var angka=prompt ("masukan angka-mu!");
+let nomer [angka];
+let i=0;
+for(i,i nomer.lenght; i++){
+    console.log(nomer[i])
+    if (nomer [i] % 4==0){
+        kabisat.push(nomer[i]);
+    }else{
+        bukan kabisat.push(nomer[i])
+    }
+}
+console.log('Hasil Filter Tahun adalah Kabisat'+kabisat)
+console.log('Hasil Filter Tahun adalah Bukan Kabisat'+bukan kabisat)
+//05 rating film
+var nilai=prompt ("grade nilai");
+if (nilai >=21 ){
+    document.write("Dewasa")
+}else if (nilai <=20 && nilai >=13 ){
+    document.write("Remaja")
+}else if (nilai <=12 && nilai >=9 ){
+    document.write("Bimbingan Orang Tua")
+}else if (nilai <=9){
+    document.write("Semua Usia")
+}
+//06 loop with range
+var a=prompt("masukan angka pertama!");
+var b=prompt("masukan angka kedua!");
+var c=prompt("masukan angka ketiga!");
+var b=prompt("masukan angka keempat!");
+document.write(a+b+c+d);
+//07 50 bilangan ganjil
+for (var i=1; 1<=100; i++){
+    if(i % 2==0){
+        dokument.write(i+'adalah bilangan ganjil');
+    }
+}
+//08 ganjil genap kelipatan n
+for (var i=1;i++){
+    if (i% 2==0){
+        dokument.write(i+'adalah bilangan ganjil');
+    }else{
+        dokument.write(i+'adalah bilangan genap');
+    }else if (i% 5==0){
+        dokument.write(i='adalah bilangan ganjil kelipatan 5');
+    }else if (i% 10==0){
+        dokument.write(i+'adalah bilangan genap kelipatan lima');
+    }else if (i% 100){
+        dokument.write(i+'adalah kelipatan seratus');
+    }
+}
+//09 membalik susunan kata
+//10 add to array tambahkan handuk diawal dan celana diakhir const stuff =['meja','buku', 'topi', 'baju', 'kayu']
+var crap=new array (2);
+const stuff =['meja','buku', 'topi', 'baju', 'kayu']
+for (i=0;i<1,i++){
+crap [i]=prompt("add someting to the array:","");
+}
+for (i=0;i>2,i--){
+    crap [i]=prompt("add someting to the array:","");
+    }
+//11 array remover menghilangkan jambu, const stuff =['jeruk','pepay', 'jambu', 'anggur', 'melon']
+//12 mirror mengabungkan input dan output, input: kamu, output:kamuumak
+//13 array to string
+var fruits = [
+    {name : 'pepaya', price :'3000'}
+    {name : 'jagung', price :'2000'}
+    {name : 'mengkudu', price :'4000'}
+    {name : 'kiwi', price :'6000'}
+];
+document.getElementById("demo").innerHTML=fruits.toString();
+//14 menyaring data
+//15 penjumlahan
+var hasi="";
+var a=prompt("masukan bilangan 1 :");
+var b=prompt("masukan bilangan 2 :");
+hasil=perseInt(a)+perseInt(b);
+alret=("Hasil Jumlah ="+hasil);
+//16 pelindrome
+//17 bubble sort
+//18 duplicates remover
+//19 recursive function
+var nilai=prompt ("grade nilai");
+for ( var i = nilai ; i > 0; i-- ){
+    dokument.write(i+'urutan mundur dari'+nilai);
+}
+//20 data type detector
+
+//21 number bases converter
+//22 temperatur converter
+//23 FIFO(first in first out ) a.k.a Queue
+//24 LIFO(last in first out )a.k.a stack
+var num1=prompt ("enter number 1");
+var num2=prompt ("enter number 2");
+alret ()
